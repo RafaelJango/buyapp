faf973912a18d846f9d9a66f24f02937332cec28
Jango <rafaeljangodev@gmail.com>
2025-06-17 00:33:36 -0300

    feat: Input

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,14 +1,21 @@
 import { View, Image } from "react-native";
-import { Styles } from "./styles";
+import { styles } from "./styles";
 
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 
 export default function Home() {
   return (
-    <View style={Styles.container}>
-      <Image source={require("@/assets/logo.png")} style={Styles.Logo} />
-      <Button title="Entrar" />
-      <Button title="Sair" />
+    <View style={styles.container}>
+      <Image source={require("@/assets/logo.png")} style={styles.logo} />
+      <View style={styles.form}>
+        <Input placeholder="Oque você precisa comprar?" />
+        <Button title="Entrar" />
+      </View>
+
+      <View style={styles.content}>
+
+      </View>
     </View>
   );
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -1,16 +1,29 @@
 import { StyleSheet } from "react-native";
 
-export const Styles = StyleSheet.create({
+export const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#D0D2D8",
     alignItems: "center",
     paddingTop: 62,
-    padding: 24,
-    gap: 24,
   },
-  Logo: {
+  logo: {
     height: 34,
     width: 134,
+  },
+  form: {
+    width: "100%",
+    paddingHorizontal: 16,
+    gap: 7,
+    marginTop: 42,
+  },
+  content: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#FFFFFF",
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+    padding: 24,
+    marginTop: 24,
   },
 });
